5136abd38437e573bb850688bd648dae71a4c967
Simon Zhao <simon.zhao@codecanteen.com>
2017-01-03 23:48:22 +1100

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularScssApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoAngularScssApp
+ */
+angular.module('yoAngularScssApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularScssApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoAngularScssApp
+ */
+angular.module('yoAngularScssApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
